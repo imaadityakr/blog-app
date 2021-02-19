b5c31959ff49d43721555c44ed5d35637dc3b335
Aaditya Kumar Dixit <imaadityakrdixit@gmail.com>
2021-02-19 17:45:02 +0530

    Initial commit

---
+++ mysite/blog/feeds.py
@@ -0,0 +1,21 @@
+from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords
+from django.urls import reverse_lazy
+from .models import Post
+
+class LatestPostsFeed(Feed):
+	title = 'My blog'
+	link = reverse_lazy('blog:post_list')
+	description = 'New posts of my blog.'
+
+	def items(self):
+		return Post.published.all()[:5]
+		print("item")
+
+	def item_title(self, item):
+		print("ITEM",item)
+		return item.title
+
+	def item_description(self, item):
+		print("BODY",item)
+		return truncatewords(item.body, 30)
